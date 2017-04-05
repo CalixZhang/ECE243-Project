f9c8ddb6cf04c6796ba8afcdf2f812e55f7093bb
Mith15 <danlu.liu1997@gmail.com>
2017-04-05 17:38:30 -0400

    Add files via upload
    Completed vga_display.s
    ASCII codes for chars in display separate in ascii_codes.s

---
+++ ascii_codes.s
@@ -0,0 +1,150 @@
+
+######################################################################
+#### ALL ASCII CODES FOR CHARACTERS USED
+######################################################################
+
+.global DOT
+.global DASH
+.global SPACE
+.global GREATER
+.global COLON
+
+.global NUM_0
+.global NUM_1
+.global NUM_2
+.global NUM_3
+.global NUM_4
+.global NUM_5
+.global NUM_6
+.global NUM_7
+.global NUM_8
+.global NUM_9
+.global CHAR_A
+.global CHAR_B
+.global CHAR_C
+.global CHAR_D
+.global CHAR_E
+.global CHAR_F
+.global CHAR_G
+.global CHAR_H
+.global CHAR_I
+.global CHAR_J
+.global CHAR_K
+.global CHAR_L
+.global CHAR_M
+.global CHAR_N
+.global CHAR_O
+.global CHAR_P
+.global CHAR_Q
+.global CHAR_R
+.global CHAR_S
+.global CHAR_T
+.global CHAR_U
+.global CHAR_V
+.global CHAR_W
+.global CHAR_X
+.global CHAR_Y
+.global CHAR_Z
+
+.global CHAR_a
+.global CHAR_b
+.global CHAR_c
+.global CHAR_d
+.global CHAR_e
+.global CHAR_f
+.global CHAR_g
+.global CHAR_h
+.global CHAR_i
+.global CHAR_j
+.global CHAR_k
+.global CHAR_l
+.global CHAR_m
+.global CHAR_n
+.global CHAR_o
+.global CHAR_p
+.global CHAR_q
+.global CHAR_r
+.global CHAR_s
+.global CHAR_t
+.global CHAR_u
+.global CHAR_v
+.global CHAR_w
+.global CHAR_x
+.global CHAR_y
+.global CHAR_z
+
+
+#################################################################
+#### ASCII CODES
+#################################################################
+
+.equ DOT, 0x2e
+.equ DASH, 0x2d
+.equ SPACE, 0x20
+.equ GREATER, 0x3e
+.equ COLON, 0x3a
+
+.equ NUM_0, 0x30
+.equ NUM_1, 0x31
+.equ NUM_2, 0x32
+.equ NUM_3, 0x33
+.equ NUM_4, 0x34
+.equ NUM_5, 0x35
+.equ NUM_6, 0x36
+.equ NUM_7, 0x37
+.equ NUM_8, 0x38
+.equ NUM_9, 0x39
+
+.equ CHAR_A, 0x41
+.equ CHAR_B, 0x42
+.equ CHAR_C, 0x43
+.equ CHAR_D, 0x44
+.equ CHAR_E, 0x45
+.equ CHAR_F, 0x46
+.equ CHAR_G, 0x47
+.equ CHAR_H, 0x48
+.equ CHAR_I, 0x49
+.equ CHAR_J, 0x4a
+.equ CHAR_K, 0x4b
+.equ CHAR_L, 0x4c
+.equ CHAR_M, 0x4d
+.equ CHAR_N, 0x4e
+.equ CHAR_O, 0x4f
+.equ CHAR_P, 0x50
+.equ CHAR_Q, 0x51
+.equ CHAR_R, 0x52
+.equ CHAR_S, 0x53
+.equ CHAR_T, 0x54
+.equ CHAR_U, 0x55
+.equ CHAR_V, 0x56
+.equ CHAR_W, 0x57
+.equ CHAR_X, 0x58
+.equ CHAR_Y, 0x59
+.equ CHAR_Z, 0x5a
+
+.equ CHAR_a, 0x61
+.equ CHAR_b, 0x62
+.equ CHAR_c, 0x63
+.equ CHAR_d, 0x64
+.equ CHAR_e, 0x65
+.equ CHAR_f, 0x66
+.equ CHAR_g, 0x67
+.equ CHAR_h, 0x68
+.equ CHAR_i, 0x69
+.equ CHAR_j, 0x6a
+.equ CHAR_k, 0x6b
+.equ CHAR_l, 0x6c
+.equ CHAR_m, 0x6d
+.equ CHAR_n, 0x6e
+.equ CHAR_o, 0x6f
+.equ CHAR_p, 0x70
+.equ CHAR_q, 0x71
+.equ CHAR_r, 0x72
+.equ CHAR_s, 0x73
+.equ CHAR_t, 0x74
+.equ CHAR_u, 0x75
+.equ CHAR_v, 0x76
+.equ CHAR_w, 0x77
+.equ CHAR_x, 0x78
+.equ CHAR_y, 0x79
+.equ CHAR_z, 0x7a
--- vga_display.s
+++ vga_display.s
@@ -1,11 +1,307 @@
-.equ ADDR_VGA, 0x08000000	# pixel buffer
-.equ ADDR_CHAR, 0x09000000	# char buffer
 
+.equ ADDR_CHAR, 0x09000000
+.equ ADDR_PUSHBUTTONS, 0xFF200050
+
+###############################################################
+#### DATA SECTION
+###############################################################
+
+.data
+indicator_offset:
+	.word 1290
+	.word 1291
+	.word 1292
+
+.text
+###############################################################
+#### DRAWING START
+###############################################################
 .global _start
 _start:
-	movia r2, ADDR_VGA
+	
 	movia r3, ADDR_CHAR
-	movui r4, 0xffff
-	movi r5, 0x41
-	sthio r4, 1032(r2)
-	stbio r5, 132(r3)
+	movi r6, 0		# offset counter
+	movi r7, 7632 	# max offset
+
+CLEAR_SCREEN:
+	# Clears entire screen
+	movi r5, SPACE
+	stbio r5, 0(r3)
+	addi r3, r3, 1
+	addi r6, r6, 1
+	bne r6, r7, CLEAR_SCREEN
+
+	
+DRAW_TITLE:
+	# write title "GUITAR PLAYER" to top of display
+	# first character displayed at (33, 2)
+	movia r3, ADDR_CHAR
+	movi r5, CHAR_G
+	stbio r5, 289(r3)
+	movi r5, CHAR_U
+	stbio r5, 290(r3)
+	movi r5, CHAR_I
+	stbio r5, 291(r3)
+	movi r5, CHAR_T
+	stbio r5, 292(r3)
+	movi r5, CHAR_A
+	stbio r5, 293(r3)
+	movi r5, CHAR_R
+	stbio r5, 294(r3)
+	
+	movi r5, SPACE
+	stbio r5, 295(r3)
+	
+	movi r5, CHAR_P
+	stbio r5, 296(r3)
+	movi r5, CHAR_L
+	stbio r5, 297(r3)
+	movi r5, CHAR_A
+	stbio r5, 298(r3)
+	movi r5, CHAR_Y
+	stbio r5, 299(r3)
+	movi r5, CHAR_E
+	stbio r5, 300(r3)
+	movi r5, CHAR_R
+	stbio r5, 301(r3)
+	
+	br DRAW_PAT_INTRO
+	
+DRAW_PAT_INTRO: # "Select a Pattern:"
+	movia r3, ADDR_CHAR
+	
+	movi r5, CHAR_S
+	stbio r5, 778(r3)
+	movi r5, CHAR_e
+	stbio r5, 779(r3)
+	movi r5, CHAR_l
+	stbio r5, 780(r3)
+	movi r5, CHAR_e
+	stbio r5, 781(r3)
+	movi r5, CHAR_c
+	stbio r5, 782(r3)
+	movi r5, CHAR_t
+	stbio r5, 783(r3)
+	movi r5, SPACE
+	stbio r5, 784(r3)
+	movi r5, CHAR_A
+	stbio r5, 785(r3)
+	movi r5, SPACE
+	stbio r5, 786(r3)
+	movi r5, CHAR_P
+	stbio r5, 787(r3)
+	movi r5, CHAR_a
+	stbio r5, 788(r3)
+	movi r5, CHAR_t
+	stbio r5, 789(r3)
+	movi r5, CHAR_t
+	stbio r5, 790(r3)
+	movi r5, CHAR_e
+	stbio r5, 791(r3)
+	movi r5, CHAR_r
+	stbio r5, 792(r3)
+	movi r5, CHAR_n
+	stbio r5, 793(r3)
+	movi r5, COLON
+	stbio r5, 794(r3)
+
+DRAW_FIRST_CHOICE: 			# "1.  D - D - D - D - "
+							
+	movi r5, NUM_1
+	stbio r5, 1294(r3)
+	movi r5, DOT
+	stbio r5, 1295(r3)
+	movi r5, SPACE
+	stbio r5, 1296(r3)
+	movi r5, SPACE
+	stbio r5, 1297(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 1298(r3)
+	movi r5, SPACE
+	stbio r5, 1299(r3)
+	movi r5, DASH
+	stbio r5, 1300(r3)
+	movi r5, SPACE
+	stbio r5, 1301(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 1302(r3)
+	movi r5, SPACE
+	stbio r5, 1303(r3)
+	movi r5, DASH
+	stbio r5, 1304(r3)
+	movi r5, SPACE
+	stbio r5, 1305(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 1306(r3)
+	movi r5, SPACE
+	stbio r5, 1307(r3)
+	movi r5, DASH
+	stbio r5, 1308(r3)
+	movi r5, SPACE
+	stbio r5, 1309(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 1310(r3)
+	movi r5, SPACE
+	stbio r5, 1311(r3)
+	movi r5, DASH
+	stbio r5, 1312(r3)
+	movi r5, SPACE
+	stbio r5, 1313(r3)
+	
+DRAW_SECOND_CHOICE:	# "2.  D U D U D U D U "
+	
+	movi r5, NUM_2
+	stbio r5, 1678(r3)
+	movi r5, DOT
+	stbio r5, 1679(r3)
+	movi r5, SPACE
+	stbio r5, 1680(r3)
+	movi r5, SPACE
+	stbio r5, 1681(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 1682(r3)
+	movi r5, SPACE
+	stbio r5, 1683(r3)
+	movi r5, CHAR_U
+	stbio r5, 1684(r3)
+	movi r5, SPACE
+	stbio r5, 1685(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 1686(r3)
+	movi r5, SPACE
+	stbio r5, 1687(r3)
+	movi r5, CHAR_U
+	stbio r5, 1688(r3)
+	movi r5, SPACE
+	stbio r5, 1689(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 1690(r3)
+	movi r5, SPACE
+	stbio r5, 1691(r3)
+	movi r5, CHAR_U
+	stbio r5, 1692(r3)
+	movi r5, SPACE
+	stbio r5, 1693(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 1694(r3)
+	movi r5, SPACE
+	stbio r5, 1695(r3)
+	movi r5, CHAR_U
+	stbio r5, 1696(r3)
+	movi r5, SPACE
+	stbio r5, 1697(r3)
+	
+DRAW_THIRD_CHOICE:	# "3.  D - D U D - D U "
+	
+	movi r5, NUM_3
+	stbio r5, 2062(r3)
+	movi r5, DOT
+	stbio r5, 2063(r3)
+	movi r5, SPACE
+	stbio r5, 2064(r3)
+	movi r5, SPACE
+	stbio r5, 2065(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 2066(r3)
+	movi r5, SPACE
+	stbio r5, 2067(r3)
+	movi r5, DASH
+	stbio r5, 2068(r3)
+	movi r5, SPACE
+	stbio r5, 2069(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 2070(r3)
+	movi r5, SPACE
+	stbio r5, 2071(r3)
+	movi r5, CHAR_U
+	stbio r5, 2072(r3)
+	movi r5, SPACE
+	stbio r5, 2073(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 2074(r3)
+	movi r5, SPACE
+	stbio r5, 2075(r3)
+	movi r5, DASH
+	stbio r5, 2076(r3)
+	movi r5, SPACE
+	stbio r5, 2077(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 2078(r3)
+	movi r5, SPACE
+	stbio r5, 2079(r3)
+	movi r5, CHAR_U
+	stbio r5, 2080(r3)
+	movi r5, SPACE
+	stbio r5, 2081(r3)	
+	
+	
+DRAW_FOURTH_CHOICE:	# "4.  D - D U D - D U "
+	
+	movi r5, NUM_4
+	stbio r5, 2446(r3)
+	movi r5, DOT
+	stbio r5, 2447(r3)
+	movi r5, SPACE
+	stbio r5, 2448(r3)
+	movi r5, SPACE
+	stbio r5, 2449(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 2450(r3)
+	movi r5, SPACE
+	stbio r5, 2451(r3)
+	movi r5, DASH
+	stbio r5, 2452(r3)
+	movi r5, SPACE
+	stbio r5, 2453(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 2454(r3)
+	movi r5, SPACE
+	stbio r5, 2455(r3)
+	movi r5, DASH
+	stbio r5, 2456(r3)
+	movi r5, SPACE
+	stbio r5, 2457(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 2458(r3)
+	movi r5, SPACE
+	stbio r5, 2459(r3)
+	movi r5, CHAR_U
+	stbio r5, 2460(r3)
+	movi r5, SPACE
+	stbio r5, 2461(r3)
+	
+	movi r5, CHAR_D
+	stbio r5, 2462(r3)
+	movi r5, SPACE
+	stbio r5, 2463(r3)
+	movi r5, CHAR_U
+	movi r5, CHAR_U
+	stbio r5, 2464(r3)
+	movi r5, SPACE
+	stbio r5, 2465(r3)	
+
+DRAW_INDICATOR:
+	movi r5, GREATER
+	stbio r5, 1290(r3)	
+	stbio r5, 1291(r3)
+	stbio r5, 1292(r3)
+
+POLL_INPUT:
+	
+	br POLL_INPUT
+	
